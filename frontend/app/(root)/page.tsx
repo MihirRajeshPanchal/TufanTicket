2d1c435e61331eb974c5eaff3071116715ac06fb
jainambarbhaya1509 <jainambarbhaya1509@gmail.com>
2025-03-21 22:28:15 +0530

    participant count

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -25,7 +25,9 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
+            <h1 className="h1-bold bg-gradient-to-r from-violet-500 to-purple-800 bg-clip-text text-transparent">
+              Host, Connect, Celebrate: Your Events, Our Platform!
+            </h1>
             <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
             <div className="flex flex-col gap-4 sm:flex-row">
               <Button size="lg" asChild className="button w-full sm:w-fit">

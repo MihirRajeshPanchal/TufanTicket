1a7290651809aa70e5d8fc386119bcd1894ef32b
Mihir Panchal <mihirpanchal5400@gmail.com>
2025-03-21 22:46:48 +0530

    App initialised

---
+++ app/app/index.tsx
@@ -0,0 +1,27 @@
+import { WebView } from 'react-native-webview';
+import { StyleSheet, View } from 'react-native';
+
+
+export default function Home() {
+  return (
+    <>
+        <View style={styles.webviewContainer}>
+          <WebView 
+            source={{ uri: 'https://recursive-depression-d2-k.vercel.app/' }}
+            style={styles.webview}
+          />
+        </View>
+    </>
+  );
+}
+ 
+const styles = StyleSheet.create({
+  webviewContainer: {
+    flex: 1,
+    width: '100%',
+    height: 400,
+  },
+  webview: {
+    flex: 1,
+  },
+});

09f0bfcd6be4ccc787abc5e4c82f3b63c39bd7c5
Mihir Panchal <mihirpanchal5400@gmail.com>
2025-03-21 19:16:03 +0530

    remove unused import of IEventPhoto in EventPhotoGallery component

--- frontend/components/shared/EventPhotoGallery.tsx
+++ frontend/components/shared/EventPhotoGallery.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button'
 import { FileUploader } from './FileUploader'
 import { addEventPhotos } from '@/lib/actions/event.actions'
 import { usePathname } from 'next/navigation'
-import { IEventPhoto } from '@/types'
+
 
 interface EventPhotoGalleryProps {
   eventId: string
